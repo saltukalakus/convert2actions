c01fdab84a41f4404f102e025ac46877471faa12
Saltuk Alakus <saltuk@Saltuks-MacBook-Pro-2.local>
2023-10-31 02:46:21 +0300

    add tests

---
+++ tests/function-name.test.js
@@ -0,0 +1,52 @@
+const cv_fn = require ("../conversion-rules/function-name");
+
+
+describe("Function name conversion", () => {
+
+ test("Basic rule", () => {
+
+  const rule = `
+    function first(a, b) {
+    }
+  `;
+
+  const action = `
+    exports.onExecutePostLogin = async (event, api) => {
+   };
+  `;
+
+  const result = cv_fn.convert(rule).replace(/\s+/g, '')
+
+   // assertions
+   expect(result).toMatch(action.replace(/\s+/g, ''));
+
+ });
+
+ test("A rule with a nested function", () => {
+
+    const rule = `
+    function first(a, b) {
+        // Function body
+        function two(x, y, z) {
+            //console.log
+        }
+    }
+    `;
+  
+    const action = `
+      exports.onExecutePostLogin = async (event, api) => {
+        // Function body
+        function two(x, y, z) {
+            //console.log
+        }
+    }
+    `;
+  
+    const result = cv_fn.convert(rule).replace(/\s+/g, '')
+  
+     // assertions
+     expect(result).toMatch(action.replace(/\s+/g, ''));
+  
+   });
+
+})
